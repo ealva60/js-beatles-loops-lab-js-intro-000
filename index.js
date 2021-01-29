a0743a29bc05e7b92f80b3508899779edd401ad1
Erick A <servinfma@gmail.com>
2021-01-29 23:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var instPlayed = []
+  for (var i = 0; i < musicians.length; i++) {
+    instPlayed.push(musicians[i] + instruments[i])
+  }
+  return instPlayed
+}// add solution here
