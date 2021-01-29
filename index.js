b77aa33357188515202ddcd1d4c1ea9ee98429e6
Erick A <servinfma@gmail.com>
2021-01-29 23:27:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ function theBeatlesPlay(musicians, instruments) {
     instPlayed.push(musicians[i] + " plays " + instruments[i])
   }
   return instPlayed
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!"
+    i++
+  }
+  return facts
 }// add solution here
